0ad6814fc8a3f68e1a96c40c7e3c0eb58630829a
katrina kampen <katrina.kampen@HP8100-18.echs.lan>
2018-12-20 14:03:48 -0500

    Listing the sounds

--- Sound Testing.py
+++ Sound Testing.py
@@ -1,3 +1,6 @@
 import playsound
 
-playsound.playsound('Sounds/Punch.mp3')
+path = 'H:\Grade. 11\Computer Science\Final Project\Sounds'
+playsound.playsound(path + '\Punch.mp3', True)
+playsound.playsound(path + '\Move.mp3', True)
+playsound.playsound(path + '')
