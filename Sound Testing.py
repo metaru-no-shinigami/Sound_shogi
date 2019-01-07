8870b08f4f74dc4303735705eef24d69b944b763
katrina kampen <katrina.kampen@HP8100-18.echs.lan>
2019-01-07 14:49:18 -0500

    Different sounds

--- Sound Testing.py
+++ Sound Testing.py
@@ -1,6 +1,6 @@
 import playsound
 
 path = 'H:\Grade. 11\Computer Science\Final Project\Sounds'
-playsound.playsound(path + '\Punch.mp3', True)
-playsound.playsound(path + '\Move.mp3', True)
-playsound.playsound(path + '')
+playsound.playsound(path + '\Punch.mp3')
+playsound.playsound(path + '\Move.mp3')
+playsound.playsound(path + '\Click.mp3')
