277b24d0f276559015f483b263294b6b63f5d376
katrina kampen <katrina.kampen@HP8100-18.echs.lan>
2019-01-09 15:24:14 -0500

    graveyard

---
+++ Announcement.py
@@ -0,0 +1,13 @@
+-375
+
+
+import turtle
+
+graveyard_1 = turtle.Turtle()
+graveyard_2 = turtle.Turtle()
+
+graveyard_1.setpos(-375, 200)
+graveyard_2.setpos(375, 200)
+
+graveyard_1.write('Player 1 Graveyard', align="center", font=("Arial", 7, "bold"))
+graveyard_2.write('Player 2 Graveyard', align="center", font=("Arial", 7, "bold"))
