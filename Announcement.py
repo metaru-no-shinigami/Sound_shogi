bb115e81dbc0e998dae67f744846085dc642beb9
katrina kampen <katrina.kampen@HP8100-11.echs.lan>
2019-01-11 15:25:09 -0500

    Graveyard titles

--- Announcement.py
+++ Announcement.py
@@ -1,13 +1,18 @@
--375
-
 
 import turtle
+
+wn = turtle.Screen()
 
 graveyard_1 = turtle.Turtle()
 graveyard_2 = turtle.Turtle()
 
+graveyard_1.penup()
+graveyard_2.penup()
+
 graveyard_1.setpos(-375, 200)
 graveyard_2.setpos(375, 200)
 
-graveyard_1.write('Player 1 Graveyard', align="center", font=("Arial", 7, "bold"))
-graveyard_2.write('Player 2 Graveyard', align="center", font=("Arial", 7, "bold"))
+graveyard_1.write('Player 1 Graveyard', align="center", font=("Arial", 20, "bold"))
+graveyard_2.write('Player 2 Graveyard', align="center", font=("Arial", 20, "bold"))
+
+wn.mainloop()
